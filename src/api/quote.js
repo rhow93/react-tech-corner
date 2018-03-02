86ff96dbad959598d3e0a98b08f5087e45cda52b
Rory How <rhow93@gmail.com>
2018-03-03 01:27:35 +0200

    update globals, don't need to specify fetch as async

--- src/api/quote.js
+++ src/api/quote.js
@@ -1,3 +1,3 @@
 const url = 'https://api.whatdoestrumpthink.com/api/v1/quotes';
 
-export default async () => fetch(url).then(response => response.json());
+export default () => fetch(url).then(response => response.json());
